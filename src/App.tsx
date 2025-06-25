4f1d4bc498a8ad5e1dd49a757c50733e93b1ede7
Bohdan <bohdanmatv1@gmail.com>
2025-06-25 16:33:04 +0300

    added basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage.tsx';
 import TodoListPage from './pages/TodoList.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
+import Navbar from './components/Navbar.tsx';
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/list" element={<TodoListPage />} />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Todo List</li>
+            </ul>
+        </div>
+    )
+}
