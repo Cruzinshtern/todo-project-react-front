99e4d3fd1d2cf182594bd50a6ee0ab3470427d04
Bohdan <bohdanmatv1@gmail.com>
2025-06-30 13:39:59 +0300

    relocate navbar into features folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage.tsx';
 import TodoListPage from './pages/TodoList.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
-import Navbar from './components/Navbar.tsx';
+import Navbar from './features/Navbar.tsx';
 import SerttingsPage from './pages/SettingsPage.tsx';
 import AuthPage from './pages/AuthPage.tsx';
 import Register from './features/auth/Register.tsx';
